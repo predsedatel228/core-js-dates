e0d42d05ff9ec00d6b1d2ba842013ad6bb939b73
Egor <predsedatel228@yandex.ru>
2024-03-23 19:20:51 +0300

    add solution for dateToTimestamp and getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return Date.parse(date);
 }
 
 /**
@@ -31,8 +31,9 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const currDate = new Date(date);
+  return `${String(currDate.getHours()).padStart(2, '0')}:${String(currDate.getMinutes()).padStart(2, '0')}:${String(currDate.getSeconds()).padStart(2, '0')}`;
 }
 
 /**
